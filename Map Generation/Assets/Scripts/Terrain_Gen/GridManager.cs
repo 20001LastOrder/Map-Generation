5a8d7cbc2aeedfc1c6cec0ea6ad7bb061783b0d9
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-10-19 14:17:14 -0400

    added basic grid rendering

--- Map Generation/Assets/Scripts/Terrain_Gen/GridManager.cs
+++ Map Generation/Assets/Scripts/Terrain_Gen/GridManager.cs
@@ -18,21 +18,35 @@ public class GridManager : MonoBehaviour
         }
 
         float offset = ((float)rootGridDim / 2.0f) * rootGridSize;
+        float halfGrid = (float)rootGridSize / 2.0f;
         Vector2 topLeftCorner = new Vector2(-offset, -offset);
+
+        Debug.Log(topLeftCorner);
 
         for(int r = 0; r < rootGridDim; r++)
         {
             List<Grid> row = new List<Grid>();
-            float row_offset = (topLeftCorner.x * r) + ((float)rootGridSize / 2.0f);
+            float row_offset = topLeftCorner.x + (rootGridSize * r) + halfGrid;
             for(int c = 0; c < rootGridDim; c++)
             {
-                float col_offset = (topLeftCorner.y * c) + ((float)rootGridSize / 2.0f);
+                float col_offset = topLeftCorner.y + (rootGridSize * c) + halfGrid;
                 GameObject subGridGO = new GameObject("subGrid_" + r + "_" + c);
                 subGridGO.transform.position = new Vector3(row_offset, 0, col_offset);
                 subGridGO.transform.parent = m_rootGrid.transform;
 
                 Grid subGrid = subGridGO.AddComponent<Grid>();
                 subGrid.m_parentGrid = m_rootGrid;
+
+                LineRenderer lineRenderer = subGridGO.AddComponent<LineRenderer>();
+                Vector3[] points = new Vector3[5];
+                points[0] = subGridGO.transform.position + new Vector3(-halfGrid, 0, -halfGrid);
+                points[1] = subGridGO.transform.position + new Vector3(-halfGrid, 0, halfGrid);
+                points[2] = subGridGO.transform.position + new Vector3(halfGrid, 0, halfGrid);
+                points[3] = subGridGO.transform.position + new Vector3(halfGrid, 0, -halfGrid);
+                points[4] = subGridGO.transform.position + new Vector3(-halfGrid, 0, -halfGrid);
+                lineRenderer.positionCount = 5;
+                lineRenderer.SetPositions(points);
+                lineRenderer.SetWidth(0.1f, 0.1f);
             }
         }
     }
