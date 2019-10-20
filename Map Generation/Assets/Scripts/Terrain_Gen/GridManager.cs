b58bf540213c4003bae8f47db2d4f924f988f354
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-10-20 13:12:01 -0400

    added basic plane rendering

--- Map Generation/Assets/Scripts/Terrain_Gen/GridManager.cs
+++ Map Generation/Assets/Scripts/Terrain_Gen/GridManager.cs
@@ -6,6 +6,7 @@ public class GridManager : MonoBehaviour
 {
     public int rootGridDim = 10;
     public int rootGridSize = 10;
+    public Material greenMaterial;
 
     Grid m_rootGrid;
     
@@ -34,9 +35,11 @@ public class GridManager : MonoBehaviour
                 subGridGO.transform.position = new Vector3(row_offset, 0, col_offset);
                 subGridGO.transform.parent = m_rootGrid.transform;
 
+                // add Grid component
                 Grid subGrid = subGridGO.AddComponent<Grid>();
                 subGrid.m_parentGrid = m_rootGrid;
 
+                // add LineRenderer component
                 LineRenderer lineRenderer = subGridGO.AddComponent<LineRenderer>();
                 Vector3[] points = new Vector3[5];
                 points[0] = subGridGO.transform.position + new Vector3(-halfGrid, 0, -halfGrid);
@@ -47,6 +50,25 @@ public class GridManager : MonoBehaviour
                 lineRenderer.positionCount = 5;
                 lineRenderer.SetPositions(points);
                 lineRenderer.SetWidth(0.1f, 0.1f);
+
+                // add plane child GO
+                GameObject planeGO = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                planeGO.transform.parent = subGridGO.transform;
+                planeGO.transform.localPosition = Vector3.zero;
+                planeGO.GetComponent<MeshRenderer>().material = greenMaterial;
+
+                Mesh mesh = planeGO.GetComponent<MeshFilter>().mesh;
+                Vector3[] verts = mesh.vertices;
+                int vertDim = (int)Mathf.Sqrt(verts.Length);
+                float[,] noise = NoiseMapGeneration.GenerateNoiseMap(vertDim, vertDim, Random.value);
+                for(int i = 0; i < vertDim; i++)
+                {
+                    for(int j = 0; j < vertDim; j++)
+                    {
+                        verts[i * vertDim + j].y = noise[i,j];
+                    }
+                }
+                mesh.vertices = verts;
             }
         }
     }
