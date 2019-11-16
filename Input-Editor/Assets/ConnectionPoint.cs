a7078953a5c993af8c7aa9af3da029402f2628ce
Dylan Havelock <dylan.havelock@gmail.com>
2019-11-16 14:24:20 -0500

    input editor first commit

---
+++ Input-Editor/Assets/ConnectionPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public enum ConnectionPointType { In, Out }
+
+public class ConnectionPoint
+{
+    public Rect rect;
+
+    public ConnectionPointType type;
+
+    public Node node;
+
+    public ConnectionPoint(Node node, ConnectionPointType type)
+    {
+        this.node = node;
+        this.type = type;
+        rect = new Rect(0, 0, 0f, 0f);
+    }
+
+    public void Draw()
+    {
+        rect.y = node.rect.y + node.rect.height * 0.5f;
+        rect.x = node.rect.x + node.rect.width * 0.5f;
+    }
+}
