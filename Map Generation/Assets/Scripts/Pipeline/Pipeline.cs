6f8d054e73a8a9066070f9637bbbc6308b46c46b
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-11-24 12:53:54 -0500

    added base Pipeline code

---
+++ Map Generation/Assets/Scripts/Pipeline/Pipeline.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipeline 
+{
+    private static List<PipelineStage> pipelineStages =
+       new List<PipelineStage>() { };
+
+    public static void execute()
+    {
+        foreach (PipelineStage stage in pipelineStages) stage.execute();
+    }
+}
---
+++ Map Generation/Assets/Scripts/Pipeline/PipelineMultiStage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class PipelineMultiStage : PipelineStage
+{
+    List<PipelineStage> stages;
+
+    public void execute()
+    {
+        Parallel.ForEach(stages, (stage) =>
+        {
+            stage.execute();
+        });
+    }
+}
