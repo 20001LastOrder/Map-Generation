6fa1215d34ebfd13fb4e9beebd56a2b82f1e65c1
msukkari <michael.sukkarieh@mail.mcgill.ca>
2020-01-29 09:03:11 -0500

    added basic terrain generation

---
+++ Map Generation/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// referrencing Brackey's GENERATING TERRAIN tutorial
+public class TerrainGenerator : MonoBehaviour
+{
+    public int depth = 20;
+
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20;
+
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        TerrainData terrainData = terrain.terrainData;
+
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+
+        float[,] height_data = new float[width, height];
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                float xCoord = (float)i / width * scale;
+                float yCoord = (float)j / height * scale;
+
+                height_data[i, j] = Mathf.PerlinNoise(xCoord, yCoord);
+            }
+        }
+
+        terrainData.SetHeights(0, 0, height_data);
+    }
+}
