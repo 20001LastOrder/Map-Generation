329b1150ebdefd1c0da68640b05ea5debb50d83c
msukkari <michael.sukkarieh@mail.mcgill.ca>
2020-02-03 09:38:43 -0500

    finished ep2 in terrain tut

---
+++ Map Generation/Assets/Scripts/Map_Gen/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth = 100;
+    public int mapHeight = 100;
+    public float noiseScale = 0.5f;
+    public bool autoUpdate;
+
+    public void generateMap()
+    {
+        float[,] noiseMap = Noise.getNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
---
+++ Map Generation/Assets/Scripts/Map_Gen/Noise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour
+{
+    public static float[,] getNoiseMap(int width, int height, float scale)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        for(int r = 0; r < height; r++)
+        {
+            for(int c = 0; c < width; c++)
+            {
+                float xCoord = (float)r / scale;
+                float yCoord = (float)c / scale;
+
+                noiseMap[r, c] = Mathf.PerlinNoise(xCoord, yCoord);
+            }
+        }
+
+        return noiseMap;
+    }
+}
